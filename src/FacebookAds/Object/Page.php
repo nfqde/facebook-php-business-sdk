01fecbaf6a94a56b9587fcf8e555c442a1af00b5
SurfAndCode <ndilthey@gmail.com>
2018-09-27 14:37:45 +0200

    Deduplicated deleteadmin function

--- src/FacebookAds/Object/Page.php
+++ src/FacebookAds/Object/Page.php
@@ -345,31 +345,6 @@ class Page extends AbstractCrudObject {
     return $pending ? $request : $request->execute();
   }
 
-  public function deleteAdMIns(array $fields = array(), array $params = array(), $pending = false) {
-    $this->assureId();
-
-    $param_types = array(
-      'trusted' => 'bool',
-      'admin_id' => 'int',
-    );
-    $enums = array(
-    );
-
-    $request = new ApiRequest(
-      $this->api,
-      $this->data['id'],
-      RequestInterface::METHOD_DELETE,
-      '/admins',
-      new AbstractCrudObject(),
-      'EDGE',
-      array(),
-      new TypeChecker($param_types, $enums)
-    );
-    $request->addParams($params);
-    $request->addFields($fields);
-    return $pending ? $request : $request->execute();
-  }
-
   public function getAdMIns(array $fields = array(), array $params = array(), $pending = false) {
     $this->assureId();
 
